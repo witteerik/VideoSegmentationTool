f9699dfaac937f6c2270a57337ad1a70e5e91bdc
witteerik <erik.witte@oru.se>
2023-02-21 10:41:22 +0100

    Adding source code for Video Segmentation Tool

---
+++ MainForm.Designer.vb
@@ -0,0 +1,342 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ImageBox1 = New Emgu.CV.UI.ImageBox()
+        Me.Next_Button = New System.Windows.Forms.Button()
+        Me.Video_TrackBar = New System.Windows.Forms.TrackBar()
+        Me.LockStart_Button = New System.Windows.Forms.Button()
+        Me.LockEnd_Button = New System.Windows.Forms.Button()
+        Me.Start_TextBox = New System.Windows.Forms.TextBox()
+        Me.End_TextBox = New System.Windows.Forms.TextBox()
+        Me.Folder_Button = New System.Windows.Forms.Button()
+        Me.FileName_ListBox = New System.Windows.Forms.ListBox()
+        Me.Extensions_TextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Previous_Button = New System.Windows.Forms.Button()
+        Me.Result_TextBox = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.TableLayoutPanel3 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Splitter4 = New System.Windows.Forms.Splitter()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Splitter3 = New System.Windows.Forms.Splitter()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.ImageBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Video_TrackBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.TableLayoutPanel3.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ImageBox1
+        '
+        Me.ImageBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.TableLayoutPanel2.SetColumnSpan(Me.ImageBox1, 4)
+        Me.ImageBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ImageBox1.Location = New System.Drawing.Point(3, 3)
+        Me.ImageBox1.Name = "ImageBox1"
+        Me.ImageBox1.Size = New System.Drawing.Size(603, 401)
+        Me.ImageBox1.TabIndex = 2
+        Me.ImageBox1.TabStop = False
+        '
+        'Next_Button
+        '
+        Me.Next_Button.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Next_Button.Location = New System.Drawing.Point(155, 450)
+        Me.Next_Button.Name = "Next_Button"
+        Me.TableLayoutPanel2.SetRowSpan(Me.Next_Button, 2)
+        Me.Next_Button.Size = New System.Drawing.Size(146, 54)
+        Me.Next_Button.TabIndex = 3
+        Me.Next_Button.Text = "Next video"
+        Me.Next_Button.UseVisualStyleBackColor = True
+        '
+        'Video_TrackBar
+        '
+        Me.TableLayoutPanel2.SetColumnSpan(Me.Video_TrackBar, 4)
+        Me.Video_TrackBar.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Video_TrackBar.Location = New System.Drawing.Point(3, 410)
+        Me.Video_TrackBar.Name = "Video_TrackBar"
+        Me.Video_TrackBar.Size = New System.Drawing.Size(603, 34)
+        Me.Video_TrackBar.TabIndex = 4
+        '
+        'LockStart_Button
+        '
+        Me.LockStart_Button.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LockStart_Button.Location = New System.Drawing.Point(307, 450)
+        Me.LockStart_Button.Name = "LockStart_Button"
+        Me.LockStart_Button.Size = New System.Drawing.Size(146, 24)
+        Me.LockStart_Button.TabIndex = 5
+        Me.LockStart_Button.Text = "Set start:"
+        Me.LockStart_Button.UseVisualStyleBackColor = True
+        '
+        'LockEnd_Button
+        '
+        Me.LockEnd_Button.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LockEnd_Button.Location = New System.Drawing.Point(307, 480)
+        Me.LockEnd_Button.Name = "LockEnd_Button"
+        Me.LockEnd_Button.Size = New System.Drawing.Size(146, 24)
+        Me.LockEnd_Button.TabIndex = 6
+        Me.LockEnd_Button.Text = "Set end:"
+        Me.LockEnd_Button.UseVisualStyleBackColor = True
+        '
+        'Start_TextBox
+        '
+        Me.Start_TextBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Start_TextBox.Location = New System.Drawing.Point(459, 450)
+        Me.Start_TextBox.Name = "Start_TextBox"
+        Me.Start_TextBox.ReadOnly = True
+        Me.Start_TextBox.Size = New System.Drawing.Size(147, 23)
+        Me.Start_TextBox.TabIndex = 7
+        '
+        'End_TextBox
+        '
+        Me.End_TextBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.End_TextBox.Location = New System.Drawing.Point(459, 480)
+        Me.End_TextBox.Name = "End_TextBox"
+        Me.End_TextBox.ReadOnly = True
+        Me.End_TextBox.Size = New System.Drawing.Size(147, 23)
+        Me.End_TextBox.TabIndex = 8
+        '
+        'Folder_Button
+        '
+        Me.Folder_Button.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Folder_Button.Location = New System.Drawing.Point(3, 3)
+        Me.Folder_Button.Name = "Folder_Button"
+        Me.Folder_Button.Size = New System.Drawing.Size(227, 24)
+        Me.Folder_Button.TabIndex = 9
+        Me.Folder_Button.Text = "Select folder"
+        Me.Folder_Button.UseVisualStyleBackColor = True
+        '
+        'FileName_ListBox
+        '
+        Me.FileName_ListBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FileName_ListBox.FormattingEnabled = True
+        Me.FileName_ListBox.ItemHeight = 15
+        Me.FileName_ListBox.Location = New System.Drawing.Point(3, 85)
+        Me.FileName_ListBox.Name = "FileName_ListBox"
+        Me.FileName_ListBox.Size = New System.Drawing.Size(227, 419)
+        Me.FileName_ListBox.TabIndex = 11
+        '
+        'Extensions_TextBox
+        '
+        Me.Extensions_TextBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Extensions_TextBox.Location = New System.Drawing.Point(3, 57)
+        Me.Extensions_TextBox.Name = "Extensions_TextBox"
+        Me.Extensions_TextBox.Size = New System.Drawing.Size(227, 23)
+        Me.Extensions_TextBox.TabIndex = 12
+        Me.Extensions_TextBox.Text = ".mp4"
+        '
+        'Label1
+        '
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label1.Location = New System.Drawing.Point(3, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(227, 24)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "Allowed file extensions"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'Previous_Button
+        '
+        Me.Previous_Button.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Previous_Button.Location = New System.Drawing.Point(3, 450)
+        Me.Previous_Button.Name = "Previous_Button"
+        Me.TableLayoutPanel2.SetRowSpan(Me.Previous_Button, 2)
+        Me.Previous_Button.Size = New System.Drawing.Size(146, 54)
+        Me.Previous_Button.TabIndex = 14
+        Me.Previous_Button.Text = "Previous video"
+        Me.Previous_Button.UseVisualStyleBackColor = True
+        '
+        'Result_TextBox
+        '
+        Me.Result_TextBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Result_TextBox.Location = New System.Drawing.Point(3, 3)
+        Me.Result_TextBox.Multiline = True
+        Me.Result_TextBox.Name = "Result_TextBox"
+        Me.Result_TextBox.Size = New System.Drawing.Size(234, 501)
+        Me.Result_TextBox.TabIndex = 15
+        Me.Result_TextBox.WordWrap = False
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Folder_Button, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label1, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.FileName_ListBox, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Extensions_TextBox, 0, 2)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 4
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(233, 507)
+        Me.TableLayoutPanel1.TabIndex = 16
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.TableLayoutPanel3)
+        Me.Panel2.Controls.Add(Me.Splitter4)
+        Me.Panel2.Controls.Add(Me.TableLayoutPanel2)
+        Me.Panel2.Controls.Add(Me.Splitter3)
+        Me.Panel2.Controls.Add(Me.TableLayoutPanel1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(0, 24)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1102, 507)
+        Me.Panel2.TabIndex = 18
+        '
+        'TableLayoutPanel3
+        '
+        Me.TableLayoutPanel3.ColumnCount = 1
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.Controls.Add(Me.Result_TextBox, 0, 0)
+        Me.TableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel3.Location = New System.Drawing.Point(862, 0)
+        Me.TableLayoutPanel3.Name = "TableLayoutPanel3"
+        Me.TableLayoutPanel3.RowCount = 1
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel3.Size = New System.Drawing.Size(240, 507)
+        Me.TableLayoutPanel3.TabIndex = 20
+        '
+        'Splitter4
+        '
+        Me.Splitter4.Location = New System.Drawing.Point(852, 0)
+        Me.Splitter4.MinExtra = 10
+        Me.Splitter4.MinSize = 10
+        Me.Splitter4.Name = "Splitter4"
+        Me.Splitter4.Size = New System.Drawing.Size(10, 507)
+        Me.Splitter4.TabIndex = 19
+        Me.Splitter4.TabStop = False
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 4
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.ImageBox1, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.End_TextBox, 3, 3)
+        Me.TableLayoutPanel2.Controls.Add(Me.LockEnd_Button, 2, 3)
+        Me.TableLayoutPanel2.Controls.Add(Me.Start_TextBox, 3, 2)
+        Me.TableLayoutPanel2.Controls.Add(Me.Video_TrackBar, 0, 1)
+        Me.TableLayoutPanel2.Controls.Add(Me.LockStart_Button, 2, 2)
+        Me.TableLayoutPanel2.Controls.Add(Me.Next_Button, 1, 2)
+        Me.TableLayoutPanel2.Controls.Add(Me.Previous_Button, 0, 2)
+        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Left
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(243, 0)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 4
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(609, 507)
+        Me.TableLayoutPanel2.TabIndex = 18
+        '
+        'Splitter3
+        '
+        Me.Splitter3.Location = New System.Drawing.Point(233, 0)
+        Me.Splitter3.MinExtra = 10
+        Me.Splitter3.MinSize = 10
+        Me.Splitter3.Name = "Splitter3"
+        Me.Splitter3.Size = New System.Drawing.Size(10, 507)
+        Me.Splitter3.TabIndex = 17
+        Me.Splitter3.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System
+        Me.MenuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.MenuStrip1.Size = New System.Drawing.Size(1102, 24)
+        Me.MenuStrip1.TabIndex = 19
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1102, 531)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainForm"
+        Me.Text = "Video Segmentation Tool"
+        CType(Me.ImageBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Video_TrackBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.TableLayoutPanel3.ResumeLayout(False)
+        Me.TableLayoutPanel3.PerformLayout()
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.TableLayoutPanel2.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ImageBox1 As Emgu.CV.UI.ImageBox
+    Friend WithEvents Next_Button As Button
+    Friend WithEvents Video_TrackBar As TrackBar
+    Friend WithEvents LockStart_Button As Button
+    Friend WithEvents LockEnd_Button As Button
+    Friend WithEvents Start_TextBox As TextBox
+    Friend WithEvents End_TextBox As TextBox
+    Friend WithEvents Folder_Button As Button
+    Friend WithEvents FileName_ListBox As ListBox
+    Friend WithEvents Extensions_TextBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Previous_Button As Button
+    Friend WithEvents Result_TextBox As TextBox
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents TableLayoutPanel3 As TableLayoutPanel
+    Friend WithEvents Splitter4 As Splitter
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents Splitter3 As Splitter
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,196 @@
+﻿Imports Emgu.CV
+Imports Emgu.CV.CvEnum
+Imports Emgu.CV.Structure
+Imports Emgu.Util
+Imports System.Windows.Forms
+Imports System.Drawing
+
+
+Public Class MainForm
+
+    Private CurrentVideo As VideoCapture = Nothing
+    Private CurrentVideoFolder As String = ""
+    Private CurrentPath As String = ""
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ImageBox1.SizeMode = PictureBoxSizeMode.Zoom
+        Video_TrackBar.Minimum = 0
+
+    End Sub
+
+    Private Sub Folder_Button_Click(sender As Object, e As EventArgs) Handles Folder_Button.Click
+
+        'Getting the folder in which to look for videos
+        Dim VideoFolder As New FolderBrowserDialog
+        VideoFolder.Description = "Video folder"
+        Dim FbdResult = VideoFolder.ShowDialog()
+        If FbdResult = DialogResult.OK Then
+
+            CurrentVideoFolder = VideoFolder.SelectedPath
+
+            Dim FilePaths = System.IO.Directory.GetFiles(CurrentVideoFolder)
+
+            Dim FileNames As New List(Of String)
+
+            For Each FilePath In FilePaths
+                Dim CurrentExtension = System.IO.Path.GetExtension(FilePath)
+                Dim AllowedExtension = "." & Extensions_TextBox.Text.Trim.Replace(".", "")
+                If CurrentExtension = AllowedExtension Then
+                    FileNames.Add(System.IO.Path.GetFileName(FilePath))
+                End If
+            Next
+
+            FileName_ListBox.Items.Clear()
+            For Each FileName In FileNames
+                FileName_ListBox.Items.Add(FileName)
+            Next
+            If FileName_ListBox.Items.Count > 0 Then
+                FileName_ListBox.SelectedIndex = 0
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub FileName_ListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles FileName_ListBox.SelectedIndexChanged
+
+        If SaveResults() = False Then Exit Sub
+
+        SetNewVideoFile()
+    End Sub
+
+    Private Sub Next_Button_Click(sender As Object, e As EventArgs) Handles Next_Button.Click
+
+        If SaveResults() = False Then Exit Sub
+
+        If FileName_ListBox.SelectedIndex = FileName_ListBox.Items.Count - 1 Then
+            MsgBox("You allready display the last video!")
+            Exit Sub
+        Else
+            FileName_ListBox.SelectedIndex += 1
+        End If
+
+    End Sub
+
+    Private Sub Previous_Button_Click(sender As Object, e As EventArgs) Handles Previous_Button.Click
+
+        If SaveResults() = False Then Exit Sub
+
+        If FileName_ListBox.SelectedIndex = 0 Then
+            MsgBox("You allready display the first video!")
+            Exit Sub
+        Else
+            FileName_ListBox.SelectedIndex -= 1
+        End If
+
+    End Sub
+
+
+    Private Sub SetNewVideoFile()
+
+        If FileName_ListBox.SelectedIndex < 0 Then Exit Sub
+        If FileName_ListBox.SelectedIndex > FileName_ListBox.Items.Count - 1 Then Exit Sub
+
+        CurrentPath = System.IO.Path.Combine(CurrentVideoFolder, FileName_ListBox.SelectedItems(0))
+
+        If System.IO.File.Exists(CurrentPath) = False Then
+            MsgBox("The file " & CurrentPath & " could not be found!", MsgBoxStyle.Exclamation, "File not found!")
+            Exit Sub
+        End If
+
+        'Loading the new video
+        CurrentVideo = New Emgu.CV.VideoCapture(CurrentPath)
+        'Setting its location to 0 (probably not necessary!)
+        CurrentVideo.Set(CapProp.PosFrames, 0)
+
+        CurrentFrameRate = CurrentVideo.Get(CapProp.Fps)
+
+        'Setting the track bar limits to the number of frames
+        Video_TrackBar.Maximum = Math.Max(0, CurrentVideo.Get(CapProp.FrameCount) - 1)
+        Video_TrackBar.Value = 0
+
+        'Calling Video_TrackBar_Scroll to display the first frame
+        Video_TrackBar_Scroll(Nothing, Nothing)
+
+    End Sub
+
+    Private Sub Video_TrackBar_Scroll(sender As Object, e As EventArgs) Handles Video_TrackBar.Scroll
+
+        If CurrentVideo IsNot Nothing Then
+
+            'Getting the index of the frame to display
+            If Video_TrackBar.Value < CurrentVideo.Get(CapProp.FrameCount) Then
+                CurrentVideo.Set(CapProp.PosFrames, Video_TrackBar.Value)
+            Else
+                Exit Sub
+            End If
+
+            'Displaying the frame
+            'Try
+            Dim CurrentImage As Image(Of Bgr, Byte) = New Image(Of Bgr, Byte)(CurrentVideo.Width, CurrentVideo.Height, New Bgr(255, 0, 0))
+            CurrentVideo.Read(CurrentImage)
+            ImageBox1.Image = CurrentImage
+            ImageBox1.Update()
+            'Catch ex As Exception
+            'MsgBox(ex.ToString)
+            'End Try
+
+        End If
+
+    End Sub
+
+    Private CurrentStartFrame As Integer = -1
+    Private CurrentEndFrame As Integer = -1
+    Private CurrentFrameRate As Integer
+
+    Private Sub LockStart_Button_Click(sender As Object, e As EventArgs) Handles LockStart_Button.Click
+        CurrentStartFrame = Video_TrackBar.Value
+        Start_TextBox.Text = CurrentStartFrame
+    End Sub
+
+    Private Sub LockEnd_Button_Click(sender As Object, e As EventArgs) Handles LockEnd_Button.Click
+        CurrentEndFrame = Video_TrackBar.Value
+        End_TextBox.Text = CurrentEndFrame
+    End Sub
+
+    Private Function SaveResults() As Boolean
+
+        'Saving current results
+        If CurrentStartFrame = -1 And CurrentEndFrame = -1 Then
+            'Returns True, since there is nothing to save
+            Return True
+        ElseIf CurrentStartFrame = -1 Or CurrentEndFrame = -1 Then
+            'Warns the user that he/she may have missed a segmentation point and aborts
+            MsgBox("Did you forget 'start' or 'end' segmentation?")
+            Return False
+        End If
+
+        Dim ResultMessage As String = String.Join(", ", New List(Of String) From {
+                                                  CurrentPath,
+                                                  CurrentStartFrame,
+                                                  CurrentEndFrame,
+                                                  Math.Round(1000 * CurrentStartFrame / CurrentFrameRate),
+                                                  Math.Round(1000 * CurrentEndFrame / CurrentFrameRate)})
+
+        Result_TextBox.AppendText(ResultMessage & vbCrLf)
+
+        CurrentStartFrame = -1
+        CurrentEndFrame = -1
+        Start_TextBox.Text = ""
+        End_TextBox.Text = ""
+
+        Return True
+
+    End Function
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+
+        AboutBox1.ShowDialog()
+
+    End Sub
+
+    Private Sub Extensions_TextBox_TextChanged(sender As Object, e As EventArgs) Handles Extensions_TextBox.TextChanged
+
+    End Sub
+End Class
